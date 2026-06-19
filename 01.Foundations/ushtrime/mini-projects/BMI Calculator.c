b15985a0ffdd256b7a7db288fe74f40cd0c3e21d
Avdi Ajeti <125797610+A357i@users.noreply.github.com>
2026-06-19 12:05:57 +0200

    Add files via upload

--- 01.Foundations/ushtrime/mini-projects/BMI Calculator.c
+++ 01.Foundations/ushtrime/mini-projects/BMI Calculator.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
 
 int main() {
+    float pesha;
+    float gjatsia;
+
+    printf("Shkruaj Peshën: ");
+    scanf("%f", &pesha);
+
+    printf("Shkruaj Gjatësinë: ");
+    scanf("%f", &gjatsia);
+
+    float BMI = pesha / (gjatsia * gjatsia);
+
+    if (BMI <= 18.5)
+    {
+        printf("BMI-ja jote eshte: %.2f ---> Nënpeshë", BMI);
+    } else if (BMI <= 24.9)
+    {
+        printf("BMI-ja jote eshte %.2f ---> Normal", BMI);
+    } else if (BMI <= 29.9)
+    {
+        printf("BMI-ja jote eshte %.2f ---> Mbipeshë", BMI);
+    } else
+    {
+        printf("BMI-ja jote eshte %.2f ---> Obez", BMI);
+    }
     
 }
